5048c2fa9415735560e522f1bf18d6e2d2c7f55b
coderzh <coderzh@f99f9cb2-d77d-11de-82c3-17aed804ed57>
2009-11-23 16:26:05 +0000

    nancyblog day2 commit the first code, nice day!

---
+++ trunk/blog/models.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+#coding:utf-8
+#
+# Copyright 2009 CoderZh.com.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = 'CoderZh'
+
+from google.appengine.api import users
+from google.appengine.ext import db
+
+class Blog(db.Model):
+	title = db.StringProperty()
+	content = db.TextProperty()
+	publishdate = db.DateTimeProperty(auto_now_add=True)
+	lastmodifytime = db.DateTimeProperty()
+	tags = db.StringListProperty()
+	category = db.CategoryProperty()
+	draft = db.BooleanProperty(default=False)
+	disabled = db.BooleanProperty(default=False)
+	
+	@staticmethod
+	def get_blogs(per_page=20, page=0):
+		query = Blog.all()
+		query.filter('draft =', False)
+		query.filter('disabled =', False)
+		query.order('-publishdate')
+		return query.fetch(per_page, offset=(page*per_page))
+	
+	def get_comments(self, per_page=30, page=0):
+		query = BlogComment.all()
+		query.filter('blog =', self)
+		return query.fetch(per_page, offset=(page*per_page))
+	
+class BlogComment(db.Model):
+	content = db.TextProperty()
+	user = db.UserProperty()
+	blog = db.ReferenceProperty(Blog)
---
+++ trunk/common/view.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#coding:utf-8
+#
+# Copyright 2009 CoderZh.com.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = 'CoderZh'
+
+import os
+from admin.models import *
+import common.config as config
+
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+class BaseRequestHandler(webapp.RequestHandler):
+	def check_login(self):
+		if users.get_current_user():
+			url = users.create_logout_url(self.request.uri)
+			url_linktext = 'Logout'
+		else:
+			url = users.create_login_url(self.request.uri)
+			url_linktext = 'Login'
+	
+	def template_render(self, template_name, template_values={}):
+		theme = Settings.get_value(config.Theme_Name, config.Theme_DefaultValue)
+		directory = os.path.split(os.path.dirname(__file__))[0]
+		template_path = os.path.join(directory, config.Theme_Folder, theme, template_name)
+		self.response.out.write(template.render(template_path, template_values, debug=config.Debug))
