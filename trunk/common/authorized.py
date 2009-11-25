b5c01e6a1f3790c336ac7793ca413448076eb9e3
coderzh <coderzh@f99f9cb2-d77d-11de-82c3-17aed804ed57>
2009-11-25 16:32:09 +0000

    now, you can add view blog fihting!

---
+++ trunk/common/authorized.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#coding:utf-8
+#
+# Copyright 2009 CoderZh.com.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__author__ = 'CoderZh'
+
+
+from google.appengine.api import users
+from google.appengine.ext import db
+
+import urllib
+
+
+def role(role):
+    """
+    import authorized
+    @authorized.role("admin")
+    def get(self):
+      user = users.GetCurrentUser(self)
+      self.response.out.write('Hello, ' + user.nickname())
+    
+    """
+    def wrapper(handler_method):
+        def check_login(self, *args, **kwargs):
+            user = users.get_current_user()
+            if not user:
+                if self.request.method != 'GET':
+                    self.error(403)
+                else:
+                    self.redirect(users.create_login_url(self.request.uri))
+            elif role == "user" or (role == "admin" and users.is_current_user_admin()):
+                return handler_method(self, *args, **kwargs)
+            else:
+                if self.request.method == 'GET':
+                    self.redirect("/403.html")
+                else:
+                    self.error(403)   # User didn't meet role.
+        return check_login
+    return wrapper
